c99286d87c91f2bfbb2f9ac79da0f747a86cc80c
Dmitriy <osendiesesein@gmail.com>
2022-04-16 09:07:12 +0300

    some more fixes

--- main.js
+++ main.js
@@ -4,27 +4,23 @@ table.classList.add('table-striped')
 document.body.appendChild(table);
 let th;
 th = document.createElement('th')
-th.setAttribute('scope','col')
 th.innerHTML = 'Кол-во операций(n)'
 table.appendChild(th);
 
 th = document.createElement('th')
-th.setAttribute('scope','col')
 th.innerHTML = 'Шаг(а)'
 table.appendChild(th);
 
 th = document.createElement('th')
-th.setAttribute('scope','col')
 th.innerHTML = 'Значение функции(y1)'
 table.appendChild(th);
 
 th = document.createElement('th')
-th.setAttribute('scope','col')
 th.innerHTML = 'Значение аргумента(х1)'
 table.appendChild(th);
 
 table.setAttribute('border','2px solid black')
-// table.setAttribute('cellpadding','0')
+table.setAttribute('cellpadding','0')
 let findFunction = function(x){
     let func = Math.cos(x)+ (1/b)*Math.cos(amountOfLetters*x+1)+ (1/b**2)*Math.cos((amountOfLetters**2)*x+2)+ (1/b**3)*Math.cos(amountOfLetters*x+3)+ (1/b**4)*Math.cos(amountOfLetters*x+4);
     return func;
@@ -61,62 +57,84 @@ while(a>aMin){
         let y2 = y1
         y2 = findFunction(x1);
         console.log(`Это n: ${n}\n Шаг: ${a}\n Значение функции(y1): ${y2}\n Аргумент(х) равен: ${x1}`);
-        
-        let tr = document.createElement('tr');
-        table.appendChild(tr);
-        let td;
 
-        td = document.createElement('td')
-   
-        td.innerHTML = n
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerHTML = a
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerHTML = y2
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerHTML = x1
-        tr.appendChild(td)
-
-        for(let i = 0; i < n;n++){
-            let datum = {
-                number:n,
-                step: a,
-                value: y2
-            }
+        let datum = {
+            number:n,
+            step: a,
+            value: y2,
+            argument: x1,
         }
+        data.push(datum);
+        
     }
     else{
         y1 = findFunction(x1);
         console.log(`Это n: ${n}\n Шаг: ${a}\n Значение функции(y1): ${y1}\n Аргумент(х) равен: ${x1}`);
 
-        let tr = document.createElement('tr');
-        table.appendChild(tr);
-        let td;
-
-        td = document.createElement('td')
- 
-        td.innerHTML = n
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerHTML = a
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerHTML = y1
-        tr.appendChild(td)
-
-        td = document.createElement('td')
-        td.innerHTML = x1
-        tr.appendChild(td)
+        let datum = {
+            number:n,
+            step: a,
+            value: y1,
+            argument: x1,
+        }
+        data.push(datum);
     }
     n++
+}
+let itemsOnPage = 10;
+let items = document.body.querySelectorAll('#pagination li')
+for(let item of items) {
+    item.addEventListener('click', function(){
+        let pageNum = +this.innerHTML
+        let lastNumber = (pageNum * 10) ;
+        let firstNumber = lastNumber - 10
+
+
+        let notes = data.slice(firstNumber, lastNumber)
+        table.innerHTML = '';
+        th = document.createElement('th')
+        th.setAttribute('scope','col')
+        th.innerHTML = 'Кол-во операций(n)'
+        table.appendChild(th);
+
+        th = document.createElement('th')
+        th.setAttribute('scope','col')
+        th.innerHTML = 'Шаг(а)'
+        table.appendChild(th);
+        
+        th = document.createElement('th')
+        th.setAttribute('scope','col')
+        th.innerHTML = 'Значение функции(y1)'
+        table.appendChild(th);
+
+        th = document.createElement('th')
+        th.setAttribute('scope','col')
+        th.innerHTML = 'Значение аргумента(х1)'
+        table.appendChild(th);
+        for(let note of notes){
+            let tr = document.createElement('tr')
+            table.appendChild(tr)
+            let td;
+
+            th = document.createElement('th')
+            th.innerHTML = note.number;
+            tr.appendChild(th)
+
+            td = document.createElement('td')
+            td.innerHTML = note.step
+            tr.appendChild(td)
+
+            td = document.createElement('td')
+            td.innerHTML = note.value
+            tr.appendChild(td)
+
+            td = document.createElement('td')
+            td.innerHTML = note.argument
+            tr.appendChild(td)
+            
+        }
+        console.log(notes)
+    })
 }
 
 
